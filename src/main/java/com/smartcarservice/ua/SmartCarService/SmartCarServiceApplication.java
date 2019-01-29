6f33e73e3537341af0478cdfd587cf3efcf017cf
Inesa09 <inesusja@gmail.com>
2019-01-29 02:49:10 +0200

    revert

--- src/main/java/com/smartcarservice/ua/SmartCarService/SmartCarServiceApplication.java
+++ src/main/java/com/smartcarservice/ua/SmartCarService/SmartCarServiceApplication.java
@@ -3,8 +3,6 @@ package com.smartcarservice.ua.SmartCarService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Arrays;
-
 @SpringBootApplication
 public class SmartCarServiceApplication {
 
